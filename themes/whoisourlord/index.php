bbce55497d16e715d0d5467075537b357ae17c1e
teenmeem <Mubashir@example.com>
2018-02-12 17:11:38 +0500

    psd o html, navigation done

---
+++ themes/whoisourlord/index.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Who is Our Lord</title>
+	<link rel="stylesheet" type = "text/css" href="style.css">
+</head>
+<body>
+	<div id ="header">
+	    <h1><a href="#">Who is Our Lord</a></h1>
+		<ul>
+			<li><a href="#">HOME</a></li>
+			<li><a href="#">CATEGORIES</a></li>
+			<li><a href="#">LEARNING</a></li>
+			<li><a href="#">ABOUT</a></li>
+			<li id = "search">
+			    <img src="img/search_icon.png" alt="Enter search item" />
+			    <input type="text" name = "s" id ="s" value = "search" />
+			</li>
+		</ul>
+	</div> !--header -->
+</body>
+</html>
