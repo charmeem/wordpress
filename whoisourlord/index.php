9bbee0bb849b42584145395328c57f91b34cc343
teenmeem <Mubashir@example.com>
2018-02-22 15:55:26 +0500

    html done

--- whoisourlord/index.php
+++ whoisourlord/index.php
@@ -15,9 +15,81 @@
 			<li><a href="#">ABOUT</a></li>
 			<li id = "search">
 			    <img src="img/search_icon.png" alt="Enter search item" />
-			    <input type="text" name = "s" id ="s" value = "search" />
+			    <input type="text" name = "s" id ="s" value = "search the site" />
 			</li>
 		</ul>
-	</div> !--header -->
+	</div> <!--end header -->
+	
+	<div id ="hero">
+	    <h1><a href="http://localhost/psd_to_html_/whoisourlord/inner1.php"> Who is Our Lord</a></h1>
+		<p>All praise be to Allah. <br>The Lord of the Universe. <br>The most Merciful, the ever Merciful. <br>Master of the day of judgment.</p>
+		
+		<div id ="player">
+		    <p>In the name of Allah, <br>the most Merciful, the <br> ever Merciful.</p>
+			<p>بِسْمِ اللّٰهِ الرَّحْمٰنِ الرَّحِيْم</p>
+		</div> <!-- end player -->
+	</div> <!-- end hero -->
+	
+	<div id = "centre">
+	    <div class="article">
+	        <h2>Who is our Lord</h2>
+            <img src="img/brushg1.png" alt="first article" />
+            <p class ="caption">The Lord of the Universe. <br>The most Merciful, the ever Merciful. <br>Master of the day of judgment.</p>		
+		</div> <!-- end article -->
+		
+		<div class="article">
+	        <h2>Who is Jesus Christ</h2>
+            <img src="img/imgg2.png" alt="second article" />
+            <p class ="caption">The Lord of the Universe. <br>The most Merciful, the ever Merciful.</p>	
+		</div> <!-- end article -->
+		
+		<div class="article">
+	        <img src="img/imgg3.png" alt="third article" />
+            <p class ="caption">And recite to them the news of Abraham.<br>when he said to His father</p>		
+		</div> <!-- end article -->
+		
+		<div class="article">
+	        <img src="img/imgg4.png" alt="fourth article" />
+            <p class ="caption">The most Merciful, the ever Merciful. <br>Master of the day of judgement.</p>		
+		</div> <!-- end article -->
+		
+		<div class="article">
+	        <h2>Who is our Lord</h2>
+            <img src="img/imgg5.png" alt="fifth article" />
+            <p class ="caption">The Lord of the Universe.<br>The most Merciful, the ever Merciful. </p>	
+		</div> <!-- end article -->
+		
+		<div class="article">
+	        <h2>Who is our Lord</h2>
+            <img src="img/brushg6.png" alt="sixth article" />
+            <p class ="caption">The Lord of the Universe. <br>The most Merciful, the ever Merciful. <br>Master of the day of judgment.</p>		
+		</div> <!-- end article -->
+		
+	</div> <!-- end center-->
+	
+	<div id = "lower">
+	    <div class="article">
+	        <h2>Who is our Lord</h2>
+            <img src="img/imgg5.png" alt="1st article" />
+        </div> <!-- end article -->
+		<div class="article">
+	        <h2>Who is Jesus Christ</h2>
+            <img src="img/imgg2.png" alt="2nd article" />
+        </div> <!-- end article -->
+		<div class="article">
+	        <h2>Who is our Lord</h2>
+            <img src="img/imgg2.png" alt="3rd article" />
+        </div> <!-- end article -->
+		<div class="article">
+	        <h2>Who is Jesus Christ</h2>
+            <img src="img/imgg2.png" alt="4rth article" />
+        </div> <!-- end article -->
+		
+	</div> <!-- end lower -->
+	
+	<div id = "footer">
+	</div> <!-- end footer -->
+	
+	
 </body>
 </html>
